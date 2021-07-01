5ab73dfc768119ae47fedaa4c979ce164b872e02
nitgood07 <nitgood07@yahoo.co.in>
2021-07-01 10:30:11 +0530

    Interfaces for book operations added

---
+++ Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface IBookRepository:IRepositoryBase<Book>
+    {
+        IEnumerable<Book> GetAllBooks();
+        Book GetBookById(int bookId);
+        Book GetBookByAuthor(int authorId);
+        void CreateBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(Book book);
+    }
+}
---
+++ Interfaces/IRepositoryWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface IRepositoryWrapper
+    {
+        IBookRepository Book { get; }
+        void Save();
+    }
+}
